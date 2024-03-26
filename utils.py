5b651dd2af7df5b20866e9f223ec29f8dda875a7
Kevin Buhler <buhlerkw@gmail.com>
2024-03-26 00:08:44 -0600

    26 common logical laws

--- utils.py
+++ utils.py
@@ -2,5 +2,8 @@ from dataclasses import dataclass
 import ast
 
 @dataclass
-class booleanTree:
-    tree: ast
+class BooleanTree:
+    tree: ast.AST
+
+print("x or y:", ast.dump(ast.parse('x or y', mode='eval'), indent=4))
+
